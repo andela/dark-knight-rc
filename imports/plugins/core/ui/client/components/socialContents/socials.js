3e4e3184bb3c83d4804cfa81a35576056f8e3e4a
Anaeze Nsoffor <andeladeveloper@Andelas-MacBook-Pro-22.local>
2018-06-06 13:14:11 +0100

    [Feature #157911836] Embed social content
    1. Remove non-working Twitter social content implementation feed due to Content Security Policy violations of twitter api
    2. Modify styling for facebook social content due to removal of twitter timeline plugin
    
    [Finishes #157911836]

--- imports/plugins/core/ui/client/components/socialContents/socials.js
+++ imports/plugins/core/ui/client/components/socialContents/socials.js
@@ -4,14 +4,14 @@ import "./style.less";
 // import classnames from "classnames/dedupe";
 import { registerComponent } from "@reactioncommerce/reaction-components";
 
-export const SocialContents = props => {
+export const SocialContents = () => {
   return (
     <div className="row">
-      <div id="fb-root" />
-      <div className="col-lg-6 col1">
+      <div className="col-md-2">&nbsp;</div>
+      <div className="col-md-8">
         <div
           className="fb-page"
-          data-href="https://www.facebook.com/tipsgalaxy/"
+          data-href="https://www.facebook.com/AliExpress/"
           data-tabs="timeline, events, messages"
           data-width="500"
           data-small-header="false"
@@ -19,18 +19,12 @@ export const SocialContents = props => {
           data-hide-cover="false"
           data-show-facepile="true"
         >
-          <blockquote cite="https://www.facebook.com/tipsgalaxy/" className="fb-xfbml-parse-ignore">
-            <a href="https://www.facebook.com/tipsgalaxy/">Tips-Galaxy</a>
+          <blockquote cite="https://www.facebook.com/AliExpress/" className="fb-xfbml-parse-ignore">
+            <a href="https://www.facebook.com/AliExpress/">Our Shop</a>
           </blockquote>
         </div>
       </div>
-      &nbsp;
-      <div className="col-lg-6 col2">
-        <a className="twitter-timeline" data-theme="dark" href="https://twitter.com/AnaezeN?ref_src=twsrc%5Etfw">
-          Tweets by AnaezeN
-        </a>
-        <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8" />
-      </div>
+      <div className="col-md-2">&nbsp;</div>
       <script>
         {(function (d, s, id) {
           const fjs = d.getElementsByTagName(s)[0];
@@ -40,14 +34,6 @@ export const SocialContents = props => {
           js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0&appId=648304275501258&autoLogAppEvents=1";
           fjs.parentNode.insertBefore(js, fjs);
         })(document, "script", "facebook-jssdk")}
-        {(function (d, s, id) {
-          const fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          const js = d.createElement(s);
-          js.id = id;
-          js.src = "https://platform.twitter.com/widgets.js";
-          fjs.parentNode.insertBefore(js, fjs);
-        })(document, "script", "twitter-wjs")}
       </script>
     </div>
   );
