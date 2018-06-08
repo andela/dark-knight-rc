7a3b791a0d99ab213f77584c9e23876fe9c9ac97
Anaeze Nsoffor <andeladeveloper@Andelas-MacBook-Pro-22.local>
2018-06-08 10:50:58 +0100

    [Fix #157911836]
    ISSUE: Facebook page content integrated stopped displaying upon navigation to other components by user
    
    SOLUTION:  Replace script for dynamic rendering of facebook page content with raw iframe generated in browser

--- imports/plugins/core/ui/client/components/socialContents/socials.js
+++ imports/plugins/core/ui/client/components/socialContents/socials.js
@@ -8,7 +8,7 @@ export const SocialContents = () => {
       <div className="col-md-2">&nbsp;</div>
       <div className="col-md-8">
         <div
-          className="fb-page"
+          className="fb-page fb_iframe_widget fb_iframe_widget_fluid"
           data-href="https://www.facebook.com/AliExpress/"
           data-tabs="timeline, events, messages"
           data-width="500"
@@ -17,22 +17,23 @@ export const SocialContents = () => {
           data-hide-cover="false"
           data-show-facepile="true"
         >
-          <blockquote cite="https://www.facebook.com/AliExpress/" className="fb-xfbml-parse-ignore">
-            <a href="https://www.facebook.com/AliExpress/">Our Shop</a>
-          </blockquote>
+          <span className="iframe-style">
+            <iframe
+              name="f2f10d3d1b133a4"
+              width="500px"
+              height="1000px"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+              scrolling="no"
+              title="fb:page Facebook Social Plugin"
+              src="https://www.facebook.com/v3.0/plugins/page.php?adapt_container_width=true&amp;app_id=648304275501258&amp;channel=http%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter%2Fr%2FEIL5DcDc3Zh.js%3Fversion%3D42%23cb%3Dfe2221dda618%26domain%3Dlocalhost%26origin%3Dhttp%253A%252F%252Flocalhost%253A3000%252Ff1fb820cf9fe8a4%26relation%3Dparent.parent&amp;container_width=660&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2FAliExpress%2F&amp;locale=en_US&amp;sdk=joey&amp;show_facepile=true&amp;small_header=false&amp;tabs=timeline%2C%20events%2C%20messages&amp;width=500"
+              className="fb-content"
+            />
+          </span>
         </div>
       </div>
       <div className="col-md-2">&nbsp;</div>
-      <script>
-        {(function (d, s, id) {
-          const fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          const js = d.createElement(s);
-          js.id = id;
-          js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0&appId=648304275501258&autoLogAppEvents=1";
-          fjs.parentNode.insertBefore(js, fjs);
-        })(document, "script", "facebook-jssdk")}
-      </script>
     </div>
   );
 };
