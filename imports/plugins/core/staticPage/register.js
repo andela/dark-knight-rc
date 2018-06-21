048e4c752c900c1e2b491e5cb60add358d5a69d4
albert85 <tmtpalbert@gmail.com>
2018-06-21 12:55:10 +0100

    commited

---
+++ imports/plugins/core/staticPage/register.js
@@ -0,0 +1,36 @@
+import { Reaction } from "/server/api";
+
+Reaction.registerPackage({
+  label: "Static-Page-View",
+  name: "reaction-static-pages-view",
+  autoEnable: true,
+  registry: [
+    {
+      route: "/static-pages/:staticPageAddress",
+      name: "static-page",
+      // permissions: ["admin", "guest", "anonymous"],
+      // audience: ["anonymous", "guest"],
+      // workflow: "coreStaticPagesWorkflow",
+      template: "staticPageDisplay"
+    }
+  ]
+  // layout: [
+  //   {
+  //     layout: "coreLayout",
+  //     workflow: "coreProductWorkflow",
+  //     collection: "StaticPages",
+  //     theme: "default",
+  //     enabled: true,
+  //     structure: {
+  //       template: "staticPageDisplay",
+  //       layoutHeader: "layoutHeader",
+  //       layoutFooter: "",
+  //       notFound: "productNotFound",
+  //       dashboardHeader: "productDetailSimpleToolbar",
+  //       dashboardControls: "productDetailDashboardControls",
+  //       dashboardHeaderControls: "",
+  //       adminControlsFooter: "adminControlsFooter"
+  //     }
+  //   }
+  // ]
+});
